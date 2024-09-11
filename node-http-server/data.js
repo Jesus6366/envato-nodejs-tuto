206aea9697bcfc56ee357522a2f1a1e2255e7f77
unknown <polibos6366@gmail.com>
2024-09-10 18:58:30 -0600

    extracting query string data

--- node-http-server/data.js
+++ node-http-server/data.js
@@ -1,13 +1,16 @@
 export default [
   {
+    id: 1,
     make: "Fender",
     model: "Strat",
   },
   {
+    id: 2,
     make: "PRS",
     model: "Starla",
   },
   {
+    id: 3,
     make: "Gibson",
     model: "Les Paul",
   },
--- node-http-server/index.js
+++ node-http-server/index.js
@@ -4,6 +4,10 @@ import guitars from "./data.js";
 const server = createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 
+  const url = new URL(req.url, "http://localhost");
+
+  const id = url.searchParams.get("id");
+
   const content = `
   <!DOCTYPE html>
 <html lang="en">
@@ -13,17 +17,30 @@ const server = createServer((req, res) => {
     <title>Document</title>
 </head>
 <body>
-<h2> My Guitars </h2>
-<ul>
-${guitars.map(createListItem).join("\n")}
-</ul>
+ ${id ? getGuitarContent(id) : createList()}
 </body>
 </html>
   `;
   res.end(content);
 });
 
-const createListItem = ({ make, model }) => `<li>${make} ${model} </li>`;
+const createList = () => `<h2> My Guitars </h2>
+<ul>
+${guitars.map(createListItem).join("\n")}
+</ul>`;
+
+const createListItem = ({ id, make, model }) =>
+  `<li><a href="?id=${id}">${make} ${model}</a> </li>`;
+
+const getGuitarContent = (id) => {
+  const guitar = guitars.find((g) => g.id == id);
+
+  if (guitar) {
+    return `<h2> ${guitar.make} ${guitar.model} </h2>`;
+  } else {
+    return `<p>Guitar does not exist </p>`;
+  }
+};
 
 server.listen(5000, () => {
   console.log(`server running on port 5000`);
